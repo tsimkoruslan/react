3335dbf088040066be0a9db598a23c8a1ad33bd8
tsimkoruslan <tsimkoruslan@gmail.com>
2023-04-13 01:24:51 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Simpsons from "./components/SimpsonsComponents/Simpsons/Simpsons";
-import RickandMorty from "./components/RickandMorty/RickandMorty";
 
 function App() {
   return (
     <div>
-      <Simpsons/>
-      <RickandMorty/>
+
     </div>
   );
 }
