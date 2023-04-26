383e1b66473490774152ac8fdeb3ab028198f157
tsimkoruslan <tsimkoruslan@gmail.com>
2023-04-26 22:52:06 +0300

    v0.1

---
+++ src/constans/urls.ts
@@ -0,0 +1,15 @@
+const baseURL = 'https://jsonplaceholder.typicode.com'
+
+const urls = {
+    posts: '/posts',
+    comments: '/comments',
+    albums: '/albums',
+    photos: '/photos',
+    todos: '/todos',
+    users: '/users'
+}
+
+export {
+    baseURL,
+    urls
+}
