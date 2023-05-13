ce22c6a751c20352ab0189d2196f974c1d53c414
tsimkoruslan <tsimkoruslan@gmail.com>
2023-05-13 14:57:00 +0300

    v0.3

--- src/components/CarForm.tsx
+++ src/components/CarForm.tsx
@@ -3,6 +3,7 @@ import {SubmitHandler, useForm} from "react-hook-form";
 import {ICar} from "../interfaces";
 import {useAppDispatch} from "../hooks";
 import {carActions} from "../redux";
+import css from './style.module.css'
 
 const CarForm = () => {
 
@@ -14,7 +15,7 @@ const CarForm = () => {
         reset()
     }
     return (
-        <form onSubmit={handleSubmit(save)}>
+        <form className={css.header} onSubmit={handleSubmit(save)}>
             <input type={"text"} placeholder={'brand'} {...register('brand')}/>
             <input type={"text"} placeholder={'year'} {...register('year')}/>
             <input type={"text"} placeholder={'price'} {...register('price')}/>
